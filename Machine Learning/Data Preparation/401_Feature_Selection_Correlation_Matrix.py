583d23daac61947f8bc3eeb175d978872efafbeb
Hussein Mazloum <humazloum@gmail.com>
2024-07-31 11:18:33 -0400

    made updates to correlation matrix file

--- Machine Learning/Data Preparation/401_Feature_Selection_Correlation_Matrix.py
+++ Machine Learning/Data Preparation/401_Feature_Selection_Correlation_Matrix.py
@@ -3,9 +3,11 @@
 ## Feature Selection using a simple Correlation Matrix
 
 
+import pandas as pd
 
+my_df = pd.read_csv("feature_selection_sample_data.csv")
 
-
+correlation_matrix = my_df.corr()
 
 
 
