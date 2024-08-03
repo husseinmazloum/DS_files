649ba07c9f37560c5bb1a7c78fe444a9d29a92b6
Hussein Mazloum <humazloum@gmail.com>
2024-08-03 09:33:38 -0400

    added file on advanced regression tree

--- Machine Learning/Model Building/104_Regression_Tree_Advanced.py
+++ Machine Learning/Model Building/104_Regression_Tree_Advanced.py
@@ -1,7 +1,7 @@
 
 
 ##############################################
-## Linear Regression - ABC Grocery Task
+## Regression Tree - ABC Grocery
 ##############################################
 
 
@@ -14,12 +14,12 @@ import pandas as pd
 import pickle
 import matplotlib.pyplot as plt
 
-from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor, plot_tree
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split, cross_val_score, KFold
 from sklearn.metrics import r2_score
 from sklearn.preprocessing import OneHotEncoder
-from sklearn.feature_selection import RFECV
+## from sklearn.feature_selection import RFECV
 
 
 ######################################################################
@@ -51,25 +51,7 @@ data_for_model.dropna(how = "any", inplace = True)
 ## Deal with Outliers
 ######################################################################
 
-outlier_investigation = data_for_model.describe()
-
-outlier_columns = ["distance_from_store", "total_sales", "total_items"]
-
-## Boxplot Approach
-
-for column in outlier_columns:
-    lower_quartile = data_for_model[column].quantile(0.25)
-    upper_quartile = data_for_model[column].quantile(0.75)
-    iqr = upper_quartile - lower_quartile
-    iqr_extended  = iqr * 2
-    
-    min_border = lower_quartile - iqr_extended
-    max_border = upper_quartile + iqr_extended
-    
-    outliers = data_for_model[(data_for_model[column] < min_border) | (data_for_model[column] > max_border)].index
-    print(f"{len(outliers)} outliers detected in column {column}")
-
-    data_for_model.drop(outliers, inplace = True)
+## No need to remove outliers for decision trees. 
 
 ######################################################################
 ## Split Input Variables and Output Variable
@@ -109,7 +91,12 @@ X_test.drop(categorical_vars, axis = 1, inplace = True)
 ## Feature Selection
 ######################################################################
 
-regressor = LinearRegression()
+## Feature Selection not really needed to improve the accuracy of the decision tree, but may help with  
+## the computational speed when reducing the amount of input variables.
+## Commented out below but we can use it if needed.
+
+"""
+regressor = DecisionTreeRegressor()
 feature_selector = RFECV(regressor)
 
 fit = feature_selector.fit(X_train, y_train)
@@ -126,12 +113,13 @@ plt.xlabel("Number of Features")
 plt.title(f"Feature Selection using RFE \n Optimal Number of Features is: {optimal_feature_count} (at score of {round(max(fit.cv_results_['mean_test_score']),4)})")
 plt.tight_layout()
 plt.show()
+"""
 
 ######################################################################
 ## Model Training
 ######################################################################
 
-regressor = LinearRegression()
+regressor = DecisionTreeRegressor(max_depth = 4, random_state = 42)
 regressor.fit(X_train, y_train)
 
 ######################################################################
@@ -160,16 +148,118 @@ num_data_points, num_input_vars = X_test.shape
 adjusted_r_squared = 1 - (1 - r_squared) * (num_data_points - 1) / (num_data_points - num_input_vars - 1)
 print(adjusted_r_squared)
 
-## Extract Model Coefficients 
+## A Demonstraion of Overfitting
 
-coefficients = pd.DataFrame(regressor.coef_)
-input_variable_names = pd.DataFrame(X_train.columns)
-summary_stats = pd.concat([input_variable_names, coefficients], axis = 1)
-summary_stats.columns = ["input_variable", "coefficient"]
+y_pred_training = regressor.predict(X_train)
+r2_score(y_train, y_pred_training)
 
-## Extract Model Intercept
+## Finding the best Max Depth
 
-regressor.intercept_
+## The Max Depth with the best accuracy isn't neccessarily the best one to choose 
+## for our model. Because this could cause over fitting. In this example, 
+## a max depth of 7 would give produce the most accurate model, but we choose a
+## max depth of 4 instead. This sacrifices a bit on accuracy, but makes our model simpler
+## simpler and easier to explain to stakeholders. This also reduces over fitting.
+
+max_depth_list = list(range(1,9))
+accuracy_scores = []
+
+for depth in max_depth_list:
+    regressor = DecisionTreeRegressor(max_depth = depth, random_state = 42)
+    regressor.fit(X_train, y_train)
+    y_pred = regressor.predict(X_test)
+    accuracy = r2_score(y_test, y_pred)
+    accuracy_scores.append(accuracy)
+
+max_accuracy = max(accuracy_scores)
+max_accuracy_idx = accuracy_scores.index(max_accuracy)
+optimal_depth = max_depth_list[max_accuracy_idx]
+
+
+## Plot of Max Depths
+
+plt.plot(max_depth_list, accuracy_scores)
+plt.scatter(optimal_depth, max_accuracy, marker = "x", color = "red")
+plt.title(f"Accuracy by Max Depth \n Optimal Tree Depth: {optimal_depth} (Accuracy: {round(max_accuracy, 4)})")
+plt.xlabel("Max Depth of Decision Tree")
+plt.ylabel("Accuracy")
+plt.tight_layout()
+plt.show()
+
+
+## Plot Our Model
+
+plt.figure(figsize=(25, 15))
+tree = plot_tree(regressor,
+                 feature_names = list(X.columns),
+                 filled = True,
+                 rounded = True,
+                 fontsize = 16)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
