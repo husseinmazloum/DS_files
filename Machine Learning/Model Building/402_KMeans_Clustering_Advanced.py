bd6fa3db0ac8aec1739f26035e28b3de09b0df8b
Hussein Mazloum <humazloum@gmail.com>
2024-08-16 09:16:33 -0400

    finished advanced template on k means clustering

--- Machine Learning/Model Building/402_KMeans_Clustering_Advanced.py
+++ Machine Learning/Model Building/402_KMeans_Clustering_Advanced.py
@@ -6,8 +6,10 @@
 
 ## Import required python packages
 
-
-
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
 ########################################################
@@ -17,72 +19,112 @@
 
 ## Import tables
 
-
+transactions = pd.read_excel("data/grocery_database.xlsx", sheet_name = "transactions")
+product_areas = pd.read_excel("data/grocery_database.xlsx", sheet_name = "product_areas")
 
 
 ## Merge on product area name
 
-
+transactions = pd.merge(transactions, product_areas, how = "inner", on = "product_area_id")
 
 
 ## Drop the non-food category
 
-
+transactions.drop(transactions[transactions["product_area_name"] == "Non-Food"].index, inplace = True)
 
 
 ## Aggregate sales at customer level (by product area)
 
-
-
+transaction_summary = transactions.groupby(["customer_id", "product_area_name"])["sales_cost"].sum()
 
 
 ## Pivot data to place product areas as columns
 
-
+transaction_summary_pivot = transactions.pivot_table(index = "customer_id",
+                                                     columns = "product_area_name",
+                                                     values = "sales_cost",
+                                                     aggfunc = "sum",
+                                                     fill_value = 0,
+                                                     margins = True,
+                                                     margins_name = "Total").rename_axis(None, axis = 1)
 
 
 ## Turn sales into % sales
 
+transaction_summary_pivot = transaction_summary_pivot.div(transaction_summary_pivot["Total"], axis = 0)
 
 
+## Drop the "total" column
 
-## Drop the total column
-
-
-
+data_for_clustering = transaction_summary_pivot.drop(["Total"], axis = 1)
 
 
+########################################################
+## Data Preparation and Cleaning
+########################################################
+
+## Check for missing values
+
+data_for_clustering.isna().sum()
+
+## Normalize Data
+
+scale_norm = MinMaxScaler()
+data_for_clustering_scaled = pd.DataFrame(scale_norm.fit_transform(data_for_clustering), columns = data_for_clustering.columns)
 
 
+########################################################
+## Use WCSS to find a good value for k
+########################################################
+
+k_values = list(range(1,10))
+wcss_list = []
+
+for k in k_values:
+    kmeans = KMeans(n_clusters = k, random_state = 42)
+    kmeans.fit(data_for_clustering_scaled)
+    wcss_list.append(kmeans.inertia_)
 
 
+## plot WCSS - to find elbow in the data
+
+plt.plot(k_values, wcss_list)
+plt.title("Within Cluster Sum of Squares (WCSS) - by k")
+plt.xlabel("k")
+plt.ylabel("WCSS Score")
+plt.tight_layout()
+plt.show()
 
 
+########################################################
+## Instantiate and fit model
+########################################################
+
+kmeans = KMeans(n_clusters = 3, random_state = 42)
+kmeans.fit(data_for_clustering_scaled)
 
 
+########################################################
+## Use cluster information
+########################################################
 
 
+## Add cluster labels to our data
+
+data_for_clustering["cluster"] = kmeans.labels_
 
 
+## Check cluster sizes
+
+data_for_clustering["cluster"].value_counts()
 
 
+########################################################
+## Profile our clusters
+########################################################
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+cluster_summary = data_for_clustering.groupby("cluster")[["Dairy", "Fruit", "Meat", "Vegetables"]].mean().reset_index()
 
 
 
